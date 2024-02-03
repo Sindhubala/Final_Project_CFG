6e18de84d0b369239d5da10a032c8f6427905b31
Sindhubala <tharsan@iqintegrity.com>
2024-02-03 15:25:14 +0000

    Preprocessing data in air_csv.csv file

--- Travel.py
+++ Travel.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('air_travel.csv')
+print(df.info())
+print(df.describe())
+print(df['Destination'].value_counts())
 
-print(df.to_string())
+#print(df.to_string())
